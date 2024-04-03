7c8de4ee00d1a4743b8a4a20f077f6b03513e1cf
Asadullokh <uzbdevit@gmail.com>
2024-04-03 14:33:29 +0500

    tayyor qilib qoydim

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+require("dotenv").config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 4002;
+
+const MONGO_URL = process.env.MONGO_URL;
+
+// middlewear
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server started on ${PORT} port...`));
+  })
+  .catch((error) => console.log(error));
