d47527294387bdf884523c206501686a24da6025
Asadullokh <uzbdevit@gmail.com>
2024-04-04 15:40:28 +0500

    completed authModel

--- server/src/model/authModel.js
+++ server/src/model/authModel.js
@@ -1,44 +1,41 @@
 const mongoose = require("mongoose");
 
-const authSchema = new mongoose.Schema({
-  googleId: {
-    type: String,
-    required: true,
+const authSchema = new mongoose.Schema(
+  {
+    googleId: {
+      type: String,
+      required: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+    },
   },
-  displayName: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  surname: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-  },
-  posts: {
-    type: Array,
-    default: [],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Auth", authSchema);
