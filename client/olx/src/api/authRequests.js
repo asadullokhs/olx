76f5da3e3d22f4a7579528bda87d8323982cbb76
MuhammadBilol1011 <muhammadbilol1011@gmail.com>
2024-04-05 17:01:20 +0500

    completed api

---
+++ client/olx/src/api/authRequests.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+const API = axios.create({ baseURL: serverUrl });
+
+export const register = (formData) => API.post(`/api/auth/signup`, formData);
+export const login = (formData) => API.post(`/api/auth/login`, formData);
