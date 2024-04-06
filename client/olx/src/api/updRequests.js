688b6727866d8bf1779dd0933c22717d5f0485cc
MuhammadBilol1011 <muhammadbilol1011@gmail.com>
2024-04-06 15:57:35 +0500

    test

--- client/olx/src/api/updRequests.js
+++ client/olx/src/api/updRequests.js
@@ -5,6 +5,7 @@ const serverUrl = process.env.REACT_APP_SERVER_URL;
 const API = axios.create({ baseURL: serverUrl });
 
 
+
 export const updateAll = (id, Data, {method}) => {
   const token = JSON.parse(localStorage.getItem("token"));
   return API.put(`/api/${method}/:${id}`,Data, { headers: { token } });
