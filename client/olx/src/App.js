6f67a25d3bef2c34023f0d42d2666fb6ac784317
Asadullokh <uzbdevit@gmail.com>
2024-04-08 15:02:50 +0500

    added smth

--- client/olx/src/App.js
+++ client/olx/src/App.js
@@ -1,14 +1,17 @@
 import { useInfoContext } from "./context/Context";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const currentUser = useInfoContext()
-  return <div className="App">
-    <Routes>
-      <Route path={"/"} element={currentUser ? <Home /> : <Auth />} />
-    </Routes>
-  </div>;
+  const currentUser = useInfoContext();
+  return (
+    <div className="App">
+      <Routes>
+        <Route path={"/"} element={!currentUser ? <Home /> : <Auth />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
