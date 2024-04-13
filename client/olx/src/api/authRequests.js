3a8eed98fd2bfe1be0e2b74a02e8182776059b91
Asadullokh <uzbdevit@gmail.com>
2024-04-13 15:48:27 +0500

    tugadi

--- client/olx/src/api/authRequests.js
+++ client/olx/src/api/authRequests.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const serverUrl = process.env.REACT_APP_SERVER_URL;
+// const serverUrl = process.env.REACT_APP_SERVER_URL;
+const serverUrl = "https://server-ti8k.onrender.com";
 
 const API = axios.create({ baseURL: serverUrl });
 
