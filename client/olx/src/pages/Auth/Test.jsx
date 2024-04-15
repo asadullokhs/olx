a0254bed88a4316a233c36d17063fdb68e3ef814
Asadullokh <uzbdevit@gmail.com>
2024-04-15 17:34:27 +0500

    added toast

--- client/olx/src/pages/Auth/Test.jsx
+++ client/olx/src/pages/Auth/Test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import { toast } from "react-toastify";
 import { useInfoContext } from "../../context/Context";
 import { googleAuth } from "../../api/authRequests";
 
@@ -11,7 +12,9 @@ const Test = () => {
       <GoogleLogin
         onSuccess={async (credentialResponse) => {
           let data = jwtDecode(credentialResponse?.credential);
+          toast.loading("Please wait...");
           let res;
+
           let newUser = {
             name: data.given_name,
             surname: data.name,
@@ -19,9 +22,12 @@ const Test = () => {
             profilePicture: data.picture,
           };
           res = await googleAuth(newUser);
+          toast.dismiss();
+          console.log(res?.data);
           localStorage.setItem("profile", JSON.stringify(res?.data.findUser));
           localStorage.setItem("token", JSON.stringify(res?.data.token));
           setCurrentUser(newUser);
+          toast.success(res?.data?.message);
         }}
         onError={() => {
           console.log("Login Failed");
