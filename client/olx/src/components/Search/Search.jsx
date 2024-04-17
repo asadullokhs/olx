38f0ad057478397539348da27f78415057b65c27
Az1mbek <2g.i.d.r.a5@gmail.com>
2024-04-17 19:38:10 +0500

    added home

--- client/olx/src/components/Search/Search.jsx
+++ client/olx/src/components/Search/Search.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import "./Search.scss";
 
 const Search = () => {
-  return (
-    <div className='Search_Com'  >
-        <input type="text" placeholder='Nimani qidiryapsiz?' className='search' />
-        <input type="text" placeholder="O'zbekiston" className='location' />
-        <button className='Search_btn'> Qidruv <i class="fa-solid fa-magnifying-glass"></i></button>
-    </div>
-  )
+    return (
+        <div className="container">
+            <div className='Search_Com'  >
+                <input type="text" placeholder='Nimani qidiryapsiz?' className='search' />
+                <input type="text" placeholder="O'zbekiston" className='location' />
+                <button className='Search_btn'> Qidruv <i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </div>
+    )
 }
 
 export default Search
