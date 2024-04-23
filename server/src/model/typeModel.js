40d48229c69f3609d75438bf0f73dee4bb10f495
Asadullokh <uzbdevit@gmail.com>
2024-04-23 15:22:31 +0500

    model

---
+++ server/src/model/typeModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const typeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    subId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Sub",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Type", typeSchema);
--- server/src/model/userModel.js
+++ server/src/model/userModel.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     profilePicture: {
-      type: Object,
+      type: Object || String,
       default: null,
     },
     role: {
