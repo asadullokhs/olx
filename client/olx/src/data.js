2f874f29b284e01725a6df6f84020d4642ad1fa1
Asadullokh <uzbdevit@gmail.com>
2024-04-24 16:35:50 +0500

    togirl;adim

--- client/olx/src/data.js
+++ client/olx/src/data.js
@@ -1,54 +1,7 @@
-
-import React from 'react'
-
-export const data =[
-
-    {
-      id: "1",
-      name: "turbo",
-      img: [
-          " https://frankfurt.apollo.olxcdn.com/v1/files/b0pcpooowfu23-UZ/image;s=1000x700 ",
-          " https://frankfurt.apollo.olxcdn.com/v1/files/zbga1w0jzxxc2-UZ/image;s=1000x700 ",
-          " https://frankfurt.apollo.olxcdn.com/v1/files/ehp4vtyf5dul3-UZ/image;s=1000x700 ",
-          " https://frankfurt.apollo.olxcdn.com/v1/files/8aw57evrbvzi2-UZ/image;s=1000x700 "
-      ],
-      price: "12300$",
-      run: "3.000",
-      color: "white",
-      year: "2023",
-      engine: "1.5",
-      content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum culpa quis perferendis, fugiat sapiente dolorum?",
-      location: "Toshkent Qora-qamish 2/4",
-      phone: "+998 94 567 89 51",
-    },
-    {
-        id: "2",
-        name: "Spark turbo",
-        img: [
-            " https://frankfurt.apollo.olxcdn.com/v1/files/zbga1w0jzxxc2-UZ/image;s=1000x700 ",
-            " https://frankfurt.apollo.olxcdn.com/v1/files/8aw57evrbvzi2-UZ/image;s=1000x700 ",
-            " https://frankfurt.apollo.olxcdn.com/v1/files/ehp4vtyf5dul3-UZ/image;s=1000x700 ",
-            " https://frankfurt.apollo.olxcdn.com/v1/files/b0pcpooowfu23-UZ/image;s=1000x700 ",
-        ],
-        price: "12300$",
-        run: "25.002",
-        color: "white",
-        year: "2023",
-        engine: "1.5",
-        content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum culpa quis perferendis, fugiat sapiente dolorum?",
-        location: "Qoraqalpoq Amudaryo tumani Chimboy ",
-        phone: "+998 98 465 21 84",
-      },
-]
-=======
-import React from "react";
-
 export const data = [
   {
     id: "1",
-    name: "Spark",
+    name: "turbo",
     img: [
       " https://frankfurt.apollo.olxcdn.com/v1/files/b0pcpooowfu23-UZ/image;s=1000x700 ",
       " https://frankfurt.apollo.olxcdn.com/v1/files/zbga1w0jzxxc2-UZ/image;s=1000x700 ",
@@ -67,7 +20,7 @@ export const data = [
   },
   {
     id: "2",
-    name: "Spark",
+    name: "Spark turbo",
     img: [
       " https://frankfurt.apollo.olxcdn.com/v1/files/zbga1w0jzxxc2-UZ/image;s=1000x700 ",
       " https://frankfurt.apollo.olxcdn.com/v1/files/8aw57evrbvzi2-UZ/image;s=1000x700 ",
@@ -86,3 +39,4 @@ export const data = [
   },
 ];
 
+import React from "react";
