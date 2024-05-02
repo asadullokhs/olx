a35ecd33d43b4877f8e520b72dd6d99a573142ea
MuhammadBilol1011 <muhammadbilol1011@gmail.com>
2024-05-02 16:58:40 +0500

    set error

--- client/olx/src/api/getRequests.js
+++ client/olx/src/api/getRequests.js
@@ -8,7 +8,4 @@ export const getAll = (method) => {
   return API.get(`/api/${method}`);
 };
 
-<<<<<<< HEAD
-=======
 export const getOneProd = (id, method) => API.get(`/api/${method}/${id}`);
->>>>>>> ef1be114cb68ab5bfcf149758e9c62e3d1290df4
