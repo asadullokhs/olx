bc997b7adc98bba0da09471430ce3a2372d3edaf
Asadullokh <uzbdevit@gmail.com>
2024-05-07 16:25:56 +0500

    i've deleted a unneccesary similar functions

--- server/src/router/workRouter.js
+++ server/src/router/workRouter.js
@@ -7,6 +7,5 @@ router.get("/work", workCtrl.get);
 router.get("/work/:id", workCtrl.getOne);
 router.delete("/work/:id", authMiddlewear, workCtrl.delete);
 router.put("/work/:id", authMiddlewear, workCtrl.update);
-router.get("/works/similar", workCtrl.similar);
 
 module.exports = router;
