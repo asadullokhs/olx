bb76dfb35ca4d187d2f7578d7af48038b2151344
Asadullokh <uzbdevit@gmail.com>
2024-05-10 16:14:43 +0500

    added authMw

--- server/src/router/userRouter.js
+++ server/src/router/userRouter.js
@@ -1,10 +1,10 @@
-
 const router = require("express").Router();
 const userCtrl = require("../controller/userCtrl");
+const authMiddlewear = require("../middlewear/authMiddleware");
 
 router.get("/user", userCtrl.getUsers);
 router.get("/user/:id", userCtrl.getUserId);
-router.delete("/user/:id", userCtrl.deleteUser);
+router.delete("/user/:id", authMiddlewear, userCtrl.deleteUser);
 router.put("/user/:id", userCtrl.updateUser);
 router.put("/user/like/:id", userCtrl.like);
 
